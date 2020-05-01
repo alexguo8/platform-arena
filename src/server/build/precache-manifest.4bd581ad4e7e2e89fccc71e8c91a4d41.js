778a15cc5ea7fa60bec4aa501c27733994cf3061
Alex Guo <alexguo@Alexs-MacBook-Pro.local>
2020-05-01 14:15:41 -0400

    Move build folder

---
+++ src/server/build/precache-manifest.4bd581ad4e7e2e89fccc71e8c91a4d41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f34c15101f8812e5bf82c8b6eb2683",
+    "url": "/index.html"
+  },
+  {
+    "revision": "072a82e17e13f9331997",
+    "url": "/static/css/main.7c664604.chunk.css"
+  },
+  {
+    "revision": "5b04ef438cabb38eae5d",
+    "url": "/static/js/2.5f78969c.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.5f78969c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "072a82e17e13f9331997",
+    "url": "/static/js/main.588aa225.chunk.js"
+  },
+  {
+    "revision": "66e702fb370b5f7c4b7d",
+    "url": "/static/js/runtime-main.fb1fe6e6.js"
+  }
+]);
