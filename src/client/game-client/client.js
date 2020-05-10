899362ff08f39b9c611637c42d290f182199d720
Alex Guo <alexguo@Alexs-MacBook-Pro.local>
2020-05-10 11:47:09 -0400

    Test 3

--- src/client/game-client/client.js
+++ src/client/game-client/client.js
@@ -79,7 +79,7 @@ export class Client {
                 this.handler.player.applyInput(i);
             })
             this.handler.update(dt);
-            this.handler.player.x = Math.round(originalX + (this.handler.player.x - originalX) * 0.3);
+            this.handler.player.x = Math.round(originalX + (this.handler.player.x - originalX) * 0.1);
             this.handler.player.y = Math.round(originalY + (this.handler.player.y - originalY));
             // if (originalX - this.handler.player.x !== 0 || originalY - this.handler.player.y !== 0) {
             //     console.log([originalX - this.handler.player.x, originalY - this.handler.player.y])
